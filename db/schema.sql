29d91dbfbe495839e9977f1db0187de07eb09dce
Sandra Sanders <sandy-sanders@msn.com>
2021-12-03 14:46:22 -0700

    error check

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
+DROP TABLE IF EXISTS voters; 
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  discription TEXT
+);
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  industry_connected BOOLEAN NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );

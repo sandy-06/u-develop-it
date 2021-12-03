0f87f67ab5a571a2061bd9d8ec359c6e567f0215
Sandra Sanders <sandy-sanders@msn.com>
2021-12-03 16:27:33 -0700

    parties

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,25 @@
-DROP TABLE IF EXISTS voters; 
+DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS candidates;
+ 
+
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
-  discription TEXT
+  description TEXT
 );
 
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL,
+  industry_connected BOOLEAN NOT NULL
+ 
+);
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );

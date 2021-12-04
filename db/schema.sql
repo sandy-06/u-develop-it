4744a2d11bba424e67481757419c68fa299591b4
Sandra Sanders <sandy-sanders@msn.com>
2021-12-03 21:44:22 -0700

    databases

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,9 @@
+
+
 DROP TABLE IF EXISTS voters;
-DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
  
 
 CREATE TABLE parties (
@@ -13,7 +16,10 @@ CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
  
 );
 CREATE TABLE voters (
